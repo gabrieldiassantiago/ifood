430c5bfa43b67a39c7f054307f916a988dfb451d
Gabriel Dias <gabrieldiassantiago@hotmail.com>
2024-12-25 19:17:04 -0300

    feat: foi caarlhooooo

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,13 +1,7 @@
 'use client'
-import { usePathname } from 'next/navigation';
 import { CartProvider } from "./components/providers/cart-provider";
-import { Sidebar } from "./components/sidebar";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const isDashboardRoute = pathname.startsWith('/admin');
-  const hideSidebarRoutes = ['/register', '/login'];
-  const shouldHideSidebar = hideSidebarRoutes.includes(pathname);
 
 
 
@@ -15,7 +9,7 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
     <CartProvider>
       <div className="min-h-screen bg-gray-50">
         <div className="flex">
-          <main className={`flex-1 ${!shouldHideSidebar ? 'lg:ml-64' : ''}`}>
+          <main>
             {children}
           </main>
         </div>
